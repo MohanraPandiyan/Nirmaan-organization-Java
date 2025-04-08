a737b28aa8d54d551c06bafbbba3178ace12ea47
Mohanraj <148621099+MohanraPandiyan@users.noreply.github.com>
2025-04-08 10:16:24 +0530

    Day-3

---
+++ variableAndOperation.java
@@ -0,0 +1,14 @@
+package data;
+
+import java.util.Scanner;
+
+public class variableAndOperation {
+	public static void main(String[] args) {
+		Scanner Y = new Scanner(System.in);
+		String hello = Y.nextLine();
+		System.out.println(hello);
+		
+		
+	}
+
+}
